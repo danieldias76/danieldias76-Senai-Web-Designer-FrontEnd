1bdc7cbd8d886b835447873bc14fba2335a1b536
danieldias76 <danieldiasconceicao71@gmail.com>
2025-10-09 17:31:24 -0300

    09/10/25

--- PADARIA IMPERIAL/WEB/assets/js/script.js
+++ PADARIA IMPERIAL/WEB/assets/js/script.js
@@ -1,7 +1,14 @@
 function exibirMenu(){
     let menu = window.document.getElementsByTagName("nav")[0]
-    menu.style.display="block"
-    menu.style.backgroundColor="yellow"    
+    menu.setAttribute("class" , "exibirMenu")  
+    btMenu.style.display="none"
+    btFechar.style.display="inline"
 }
-
+function esconderMenu(){
+    let menu = window.document.getElementsByTagName("nav")[0]
+    menu.setAttribute("class", "ocultarMenu")
+    btMenu.style.display="inline"
+    btFechar.style.display="none"
+}
+btFechar.addEventListener("click", esconderMenu)
 btMenu.addEventListener("click", exibirMenu)

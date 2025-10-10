1139cbcd9b9e2097f97740f8c47ad58ac161b45a
danieldias76 <danieldiasconceicao71@gmail.com>
2025-10-10 17:31:00 -0300

    10/10/25

--- PADARIA IMPERIAL/WEB/assets/js/script.js
+++ PADARIA IMPERIAL/WEB/assets/js/script.js
@@ -1,14 +1,38 @@
-function exibirMenu(){
-    let menu = window.document.getElementsByTagName("nav")[0]
-    menu.setAttribute("class" , "exibirMenu")  
-    btMenu.style.display="none"
-    btFechar.style.display="inline"
+function exibeMenu(){
+    let menu = document.getElementsByTagName("nav")[0]
+    menu.setAttribute("class", "mostrarMenu")
+    btMenu.style.display = "none"
+    btFechar.style.display = "inline"
 }
-function esconderMenu(){
-    let menu = window.document.getElementsByTagName("nav")[0]
+function escondeMenu(){
+    let menu = document.getElementsByTagName("nav")[0]
     menu.setAttribute("class", "ocultarMenu")
-    btMenu.style.display="inline"
-    btFechar.style.display="none"
+    btMenu.style.display = "inline"
+    btFechar.style.display = "none"
 }
-btFechar.addEventListener("click", esconderMenu)
-btMenu.addEventListener("click", exibirMenu)
+btFechar.addEventListener("click", escondeMenu)
+btMenu.addEventListener("click", exibeMenu)
+
+//função para verificar o redimensionamento da tela
+
+function verificaTela(){
+    let menu = document.getElementsByTagName("nav")[0];
+    if(window.innerWidth >= 1024){
+        // Modo desktop
+        btMenu.style.display = "none";
+        btFechar.style.display = "none";
+        menu.className = "";
+        menu.style.display = "block";
+    } else {
+        // Modo mobile
+        btMenu.style.display = "block";
+        btFechar.style.display = "none";
+        menu.className = "ocultarMenu";
+        menu.style.display = "none";
+    }
+}
+
+// Verifica ao carregar a página
+window.addEventListener("resize", verificaTela);
+
+
